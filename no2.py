c07a3299ea94d0ccd074a60cd11c2d89858577e0
Willwilliamt <163222918+Willwilliamt@users.noreply.github.com>
2024-10-03 14:18:34 +0700

    Update no2.py

--- no2.py
+++ no2.py
@@ -5,14 +5,11 @@ import matplotlib.pyplot as plt
 
 
 order_items = pd.read_csv('order_items_dataset.csv')
-order_payments = pd.read_csv('order_payments_dataset.csv')
-order_reviews = pd.read_csv('order_reviews_dataset.csv')
-orders = pd.read_csv('orders_dataset.csv')
+
 products = pd.read_csv('products_dataset.csv')
 product_translation = pd.read_csv('product_category_name_translation.csv')
-sellers = pd.read_csv('sellers_dataset.csv')
-customers = pd.read_csv('customers_dataset.csv')
-geolocation = pd.read_csv('geolocation_dataset.csv')
+
+
 
 order_items_products = pd.merge(order_items, products, on='product_id')
 order_items_products = pd.merge(order_items_products, product_translation, on='product_category_name', how='left')
